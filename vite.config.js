d9ab9f7ef6c00d282d173cacfb6d607771d2d55a
irzumjafri <irzumbm@gmail.com>
2025-07-21 14:19:46 +0500

    updated config

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,7 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  define: {
+    __APP_ENV__: process.env.VITE_VERCEL_ENV,
+  },
 })
